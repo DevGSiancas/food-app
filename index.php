8c2d9448b12188178a6ce0151577033782d7589e
Gino Siancas <ginito_lee@hotmail.com>
2018-12-25 22:44:20 -0500

    commit app

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <link rel="stylesheet" href="css/index.1.0.0.css">
+</head>
+<body>
+
+  <script src="js/jquery-min.js" type="text/javascript"></script>
+  <script src="js/jquery-ui.js" type="text/javascript"></script>
+  <script src="js/index.1.0.0.js" type="text/javascript"></script>
+  <script type="text/javascript">
+    $(document).ready(function(){
+
+      alert(3);
+
+    })
+  </script>
+</body>
+</html>
